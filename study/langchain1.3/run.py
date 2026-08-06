3768cbf1f517c45ec1c40d202399b17d9eb79ddc
xs <w18320724197@163.com>
2026-08-07 00:25:58 +0800

    langchain

--- study/langchain1.3/run.py
+++ study/langchain1.3/run.py
@@ -2,9 +2,20 @@ import os
 
 from dotenv import load_dotenv
 load_dotenv()
+from langchain.chat_models import init_chat_model
 
-from utils.test_util import aa
 
+llm = init_chat_model(
+    model=os.getenv("BAILIAN_MODEL"),
+    openai_api_key=os.getenv("BAILIAN_API_KEY"),
+    openai_api_base=os.getenv("BAILIAN_BASE_URL"),
+    model_provider="openai",
+    temperature=0,
+    max_retries=3,
+    request_timeout=30,
+    streaming=True,
+)
 
-print(os.getenv("DEEPSEEK_BASE_URL"))
-print(aa)
+resp = llm.stream("你好，帮我写一段python代码，计算1+1的结果，并打印出来。")
+for chunk in resp:
+    print(chunk.content, end="", flush=True)
